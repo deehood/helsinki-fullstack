32484cdb0da391e3dda9fff0b36736663bf2dc6f
deehood <dee.hood@gmail.com>
2022-03-22 16:51:40 +0000

    2 - rendering collection

--- other-react-start/src/App.js
+++ other-react-start/src/App.js
@@ -1,5 +1,20 @@
-function App() {
-  return <div></div>;
-}
+import Note from "./components/Note";
+
+const App = ({ notes }) => {
+  console.log(notes);
+  const newNotes = notes.map((note) => note.content);
+  console.log(newNotes);
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default App;
