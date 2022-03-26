fc05e666af2352622d9c041a0c1e89de594ee8a6
deehood <dee.hood@gmail.com>
2022-03-26 21:47:12 +0000

    2.19: Phonebook step11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
+import "./styles.css";
+
 import Filter from "./components/Filter";
 import PersonList from "./components/List/PersonList";
 import PersonForm from "./components/PersonForm";
@@ -17,9 +19,12 @@ const App = () => {
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
+  const [message, setMessage] = useState(null);
+  const [isError, setIsError] = useState(false);
 
   const handleDelete = (id) => {
     const temp = persons.filter((person) => person.id !== id);
+    handleNotification("Deleted Person", false);
     setPersons(temp);
   };
 
@@ -34,6 +39,14 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const isErrorSetter = (error) => setIsError(error);
+
+  const handleNotification = (text, isError, name = null) => {
+    name !== null ? setMessage(`${text} ${name}`) : setMessage(`${text}`);
+    setTimeout(() => setMessage(null), 2000);
+    isErrorSetter(isError);
+  };
+
   const handleSubmitName = (event) => {
     event.preventDefault();
 
@@ -41,14 +54,19 @@ const App = () => {
       name: newName.trim(),
       number: newNumber.trim(),
     };
+
     //  if name already in db return index
     const index = persons.findIndex((x) => x.name === personObj.name);
 
     if (index === -1) {
-      // Person not found - create new record
-      personService
-        .create(personObj)
-        .then((returnedPerson) => setPersons(persons.concat(personObj)));
+      // Person not found - create new record  - then get new id from axios returnPerson and send to state
+      personService.create(personObj).then((returnedPerson) => {
+        // console.log(returnedPerson);
+        setPersons(persons.concat(returnedPerson));
+
+        handleNotification("Added", false, personObj.name);
+      });
+
       setNewName("");
       setNewNumber("");
     } else {
@@ -64,6 +82,7 @@ const App = () => {
           const temp = [...persons];
           temp[index].number = personObj.number;
           setPersons(temp);
+          handleNotification("Updated", false, personObj.name);
         });
 
       if (confirmed) {
@@ -73,12 +92,22 @@ const App = () => {
     }
   };
 
+  const Notification = ({ message, isError }) =>
+    isError ? (
+      <div className="message errorMessage">{message}</div>
+    ) : (
+      <div className="message normalMessage">{message}</div>
+    );
+
   return (
     <div>
       <h2>Phonebook</h2>
 
-      <Filter filter={filter} handleFilter={handleFilter} />
+      {message !== null ? (
+        <Notification message={message} isError={isError} />
+      ) : null}
 
+      <Filter filter={filter} handleFilter={handleFilter} />
       <h3>Add a new</h3>
       <PersonForm
         newName={newName}
@@ -87,7 +116,6 @@ const App = () => {
         handleChangeNumber={handleChangeNumber}
         handleSubmitName={handleSubmitName}
       />
-
       <h2>Numbers</h2>
       <PersonList
         persons={persons}
